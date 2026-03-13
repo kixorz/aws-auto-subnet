703c36aa54bcf4f6722f29e554bfc0dc45208074
Adam Konrad <git@adamkonrad.com>
2026-03-12 21:42:44 -0500

    route table support

--- auto_subnet/index.py
+++ auto_subnet/index.py
@@ -1,8 +1,13 @@
 import os
+import logging
 import boto3
-ec2 = boto3.client("ec2", region_name=os.environ['AWS_REGION'])
 
 from crhelper import CfnResource
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+ec2 = boto3.client("ec2", region_name=os.environ['AWS_REGION'])
 helper = CfnResource()
 
 
@@ -12,6 +17,8 @@ def create(event, context):
     vpc_id = properties['VpcId']
     availability_zones = properties['AvailabilityZones']
     subnets = properties['Subnets']
+    route_table_id = properties.get('RouteTableId')
+    map_public_ip = properties.get('MapPublicIpOnLaunch', 'false').lower() == 'true'
 
     stack_id = event['StackId']
     resource_id = event['LogicalResourceId']
@@ -28,9 +35,29 @@ def create(event, context):
 
     subnet_ids = []
     for az, subnet in zip(availability_zones, subnets):
-        subnet_response = ec2.create_subnet(VpcId=vpc_id, AvailabilityZone=az, CidrBlock=subnet, TagSpecifications=tag_specifications)
+        subnet_response = ec2.create_subnet(
+            VpcId=vpc_id,
+            AvailabilityZone=az,
+            CidrBlock=subnet,
+            TagSpecifications=tag_specifications,
+        )
         subnet_id = subnet_response['Subnet']['SubnetId']
         subnet_ids.append(subnet_id)
+        logger.info("Created subnet %s (%s) in %s", subnet_id, subnet, az)
+
+        if route_table_id:
+            ec2.associate_route_table(
+                RouteTableId=route_table_id,
+                SubnetId=subnet_id,
+            )
+            logger.info("Associated subnet %s with route table %s", subnet_id, route_table_id)
+
+        if map_public_ip:
+            ec2.modify_subnet_attribute(
+                SubnetId=subnet_id,
+                MapPublicIpOnLaunch={'Value': True},
+            )
+            logger.info("Enabled MapPublicIpOnLaunch on subnet %s", subnet_id)
 
     helper.Data["SubnetIds"] = subnet_ids
 
@@ -41,10 +68,27 @@ def delete(event, context):
     resource_id = event['LogicalResourceId']
     parent_resource_id = f'{stack_id}/{resource_id}'
 
-    response = ec2.describe_subnets(Filters=[{'Name': 'tag:ParentResourceId', 'Values': [parent_resource_id]}])
+    response = ec2.describe_subnets(
+        Filters=[{'Name': 'tag:ParentResourceId', 'Values': [parent_resource_id]}]
+    )
     subnets = response['Subnets']
     for subnet in subnets:
         subnet_id = subnet['SubnetId']
+
+        # Disassociate any non-main route table associations before deleting
+        rt_response = ec2.describe_route_tables(
+            Filters=[{'Name': 'association.subnet-id', 'Values': [subnet_id]}]
+        )
+        for rt in rt_response['RouteTables']:
+            for assoc in rt.get('Associations', []):
+                if assoc.get('SubnetId') == subnet_id and not assoc.get('Main', False):
+                    ec2.disassociate_route_table(
+                        AssociationId=assoc['RouteTableAssociationId']
+                    )
+                    logger.info("Disassociated route table from subnet %s", subnet_id)
+
         ec2.delete_subnet(SubnetId=subnet_id)
+        logger.info("Deleted subnet %s", subnet_id)
+
 
 handler = helper
